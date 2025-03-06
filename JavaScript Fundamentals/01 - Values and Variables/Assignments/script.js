a5f81c86e687d719ff2085fb5b7589b93828578e
Redomeister <71971064+Redomeister@users.noreply.github.com>
2025-03-06 14:36:15 +0200

    Create script.js

---
+++ JavaScript Fundamentals/01 - Values and Variables/Assignments/script.js
@@ -0,0 +1,14 @@
+/* 
+LECTURE: Values and Variables
+
+1. Declare variables called 'country', 'continent' and 'population' and
+assign their values according to your own country (population in millions)
+2. Log their values to the console
+*/
+
+
+let country, continent, population;
+country = 'America';
+continent = 'North America';
+population = 340.1;
+console.log(country, continent, population);
