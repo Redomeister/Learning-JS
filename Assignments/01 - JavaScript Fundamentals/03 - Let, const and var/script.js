c0ed567c630f7fecf8f2a1fa328a93859805dfb6
Redomeister <71971064+Redomeister@users.noreply.github.com>
2025-03-06 18:55:56 +0200

    Add files via upload

---
+++ Assignments/01 - JavaScript Fundamentals/03 - Let, const and var/script.js
@@ -0,0 +1,20 @@
+// let country = 'America';
+// let continent = 'North America';
+let population = 340.1;
+// let isIsland = false;
+let language;
+
+/*
+LECTURE: Let, const and var
+1. Set the value of 'language' to the language spoken where you live(some
+countries have multiple languages, but just choose one)
+2. Think about which variables should be const variables (which values will never
+change, and which might change ?). Then, change these variables to const.
+3. Try to change one of the changed variables now, and observe what happens
+*/
+
+language = 'English';
+const country = 'America';
+const continent = 'North America';
+const isIsland = false;
+// isIsland = true;
