99b07c21419d4f88e13cc4115e657164432dab8a
Redomeister <71971064+Redomeister@users.noreply.github.com>
2025-03-06 18:57:19 +0200

    Update script.js

--- Assignments/01 - JavaScript Fundamentals/02 - Data Types/script.js
+++ Assignments/01 - JavaScript Fundamentals/02 - Data Types/script.js
@@ -1,20 +1,9 @@
-/* 
-LECTURE: Values and Variables
-
-1. Declare variables called 'country', 'continent' and 'population' and
-assign their values according to your own country (population in millions)
-2. Log their values to the console
-*/
-
-
 let country, continent, population;
 country = 'America';
 continent = 'North America';
 population = 340.1;
-console.log(country, continent, population);
 
 /*
-LECTURE: Data Types
 1. Declare a variable called 'isIsland' and set its value according to your
 country. The variable should hold a Boolean value. Also declare a variable
 'language', but don't assign it any value yet
