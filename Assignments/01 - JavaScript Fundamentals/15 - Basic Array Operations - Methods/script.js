d35fdca79090e3fd3cd67fb9ec6caa9e587b4884
Redomeister <71971064+Redomeister@users.noreply.github.com>
2025-03-19 21:26:06 +0200

    Update script.js

--- Assignments/01 - JavaScript Fundamentals/15 - Basic Array Operations - Methods/script.js
+++ Assignments/01 - JavaScript Fundamentals/15 - Basic Array Operations - Methods/script.js
@@ -16,7 +16,6 @@ change the array at that index position. For example, you can search for
 'Sweden' in the array, and then replace it with 'Republic of Sweden'.
 */
 
-
 const neighbours = ['Canada', 'Mexico'];
 // console.log(neighbours);
 neighbours.push('Utopia');
